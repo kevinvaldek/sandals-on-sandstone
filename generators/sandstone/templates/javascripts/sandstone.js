48a3b5607338aaa50907c64441d3b0c73d7393ae
Kevin Valdek <kevinvaldek@gmail.com>
2009-09-04 22:41:00 +0200

    converted inline prototype js to unobtrusive mootools.
     * This is only to remove prototype dependency.
       Skip preview for now since this is only temporary. The fade links are probably subjects for change.

--- generators/sandstone/templates/javascripts/sandstone.js
+++ generators/sandstone/templates/javascripts/sandstone.js
@@ -1,3 +1,9 @@
 ActionChain.add({
-   
+   '.fieldset_link': function() {
+       this.fieldset = this.getNext().fade('hide');
+       this.addEvent('click', function() {
+           this.fieldset.fade('in');
+           return false;
+       });
+   }
 });

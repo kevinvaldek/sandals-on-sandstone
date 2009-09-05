9d62600d58d1dfdb46e97503b0f7a1cd2ae23063
Kevin Valdek <kevinvaldek@gmail.com>
2009-09-05 20:24:42 +0200

    make testing easier with another test runner mode.

--- generators/sandstone/templates/test/sandstone_test_helper.rb
+++ generators/sandstone/templates/test/sandstone_test_helper.rb
@@ -1,4 +1,11 @@
-require File.dirname(__FILE__) + '/test_helper'
+begin
+  # If the test is run inside an app
+  require File.dirname(__FILE__) + '/test_helper'
+rescue LoadError
+  # If the test is run from inside the vendor/plugins directory
+  require File.expand_path(File.dirname(__FILE__) + "/../../../../test/test_helper")
+end
+
 require 'rubygems'
 require 'mocha'
 require File.dirname(__FILE__) + '/sandstone_test_factory'

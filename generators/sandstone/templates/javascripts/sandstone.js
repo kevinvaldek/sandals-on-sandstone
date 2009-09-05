23bdb1f7209d12bbe7f5436b2af1709366fc01a7
Kevin Valdek <kevinvaldek@gmail.com>
2009-09-05 11:44:36 +0200

    move notice handling to js level.

--- generators/sandstone/templates/javascripts/sandstone.js
+++ generators/sandstone/templates/javascripts/sandstone.js
@@ -1,4 +1,16 @@
+var Sandals = {
+    notice: null
+};
+
 ActionChain.add({
+   '#notice': function() {
+       Sandals.notice = new Notice(this);
+       // flash notice if it contains a message on dom ready
+       if(this.get('text') != '') {
+           Sandals.notice.flash();
+       }
+   },
+
    '.fieldset_link': function() {
        this.fieldset = this.getNext().fade('hide');
        this.addEvent('click', function() {
---
+++ generators/sandstone/templates/javascripts/widgets.js
@@ -0,0 +1,37 @@
+var Notice = new Class({
+
+    Implements: Options,
+
+    options: {
+        displayTime: 5000
+    },
+
+    initialize: function(element) {
+        this.element = document.id(element);
+    },
+
+    flash: function() {
+        this.show();
+        this.fadeOut.delay(this.options.displayTime, this);
+    },
+
+    show: function() {
+        this.element.setStyle('visibility', 'visible');
+    },
+
+    fadeOut: function() {
+        this.element.set('morph', {
+            // reset
+            onComplete: function() {
+                this.element.setStyle('opacity', 1);
+                this.element.setStyle('visibility', 'hidden');
+            }.bind(this)
+        });
+        this.element.morph({ opacity: 0 });
+    },
+
+    setText: function(text) {
+        this.element.set('text', text);
+    }
+
+});
